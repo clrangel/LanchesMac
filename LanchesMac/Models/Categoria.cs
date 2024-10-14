c701b66ed07a34b779b35183a57da91aedccf24c
Camilo LR <autumndrums@hotmail.com>
2024-10-14 15:56:47 -0300

    Classes Categoria e Lanches

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,26 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set;} = new List<Lanche>();
+
+        //Usando o ICollection.
+        //public ICollection<Lanche> Lanches { get; set;} = new List<Lanche>();
+
+        public Categoria()
+        {
+        }
+
+        public Categoria(int id, string categoriaNome, string descricao)
+        {
+            Id = id;
+            CategoriaNome = categoriaNome;
+            Descricao = descricao;
+        }
+
+    }
+}
---
+++ LanchesMac/Models/Lanche.cs
@@ -0,0 +1,36 @@
+﻿namespace LanchesMac.Models
+{
+    public class Lanche
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public double Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+
+        public Lanche()
+        {
+        }
+
+        public Lanche(int id, string nome, string descricaoCurta, string descricaoDetalhada, double preco, string imagemUrl, string imagemThumbnailUrl, bool isLanchePreferido, bool emEstoque, Categoria categoria)
+        {
+            Id = id;
+            Nome = nome;
+            DescricaoCurta = descricaoCurta;
+            DescricaoDetalhada = descricaoDetalhada;
+            Preco = preco;
+            ImagemUrl = imagemUrl;
+            ImagemThumbnailUrl = imagemThumbnailUrl;
+            IsLanchePreferido = isLanchePreferido;
+            EmEstoque = emEstoque;
+            Categoria = categoria;
+        }
+    }
+}
