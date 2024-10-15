f6e0ea259844b94c966a79a3e02e5282afba6066
Camilo LR <autumndrums@hotmail.com>
2024-10-15 17:06:21 -0300

    Atualizacao na classe lanches e criacao da primeira migration e banco de dados

--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -9,33 +9,33 @@ namespace LanchesMac.Models
 
         [Required(ErrorMessage = "O nome do lanche deve ser informado")]
         [Display(Name = "Nome do Lanche")]
-        [StringLength(80, MinimumLength = 3, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2}")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
-        [Display(Name = "Descrição do lanche")]
+        [Display(Name = "Descrição do Lanche")]
         [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
-        [MaxLength(200, ErrorMessage = "Descrição deve ter no máximo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
         public string DescricaoCurta { get; set; }
 
-        [Required(ErrorMessage = "A descrição detalhada do lanche deve ser informada")]
+        [Required(ErrorMessage = "O descrição detalhada do lanche deve ser informada")]
         [Display(Name = "Descrição detalhada do Lanche")]
-        [MinLength(20, ErrorMessage = "Descrição detahada deve ter no mínimo {1} caracteres")]
-        [MaxLength(200, ErrorMessage = "Descrição detalhada deve ter no máximo {1} caracteres")]
+        [MinLength(20, ErrorMessage = "Descrição detalhada deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição detalhada pode exceder {1} caracteres")]
         public string DescricaoDetalhada { get; set; }
 
-        [Required(ErrorMessage = "Informe o valor do lanche")]
+        [Required(ErrorMessage = "Informe o preço do lanche")]
         [Display(Name = "Preço")]
-        [Column(TypeName ="double(10,2)")]
-        [Range(1.0, 999.99, ErrorMessage = "O preço deve estar entre {0} e {1}")]
-        public double Preco { get; set; }
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1, 999.99, ErrorMessage = "O preço deve estar entre 1 e 999,99")]
+        public decimal Preco { get; set; }
 
         [Display(Name = "Caminho Imagem Normal")]
-        [StringLength(200, MinimumLength = 3, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemUrl { get; set; }
 
         [Display(Name = "Caminho Imagem Miniatura")]
-        [StringLength(200, MinimumLength = 3, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemThumbnailUrl { get; set; }
 
         [Display(Name = "Preferido?")]
@@ -51,7 +51,7 @@ namespace LanchesMac.Models
         {
         }
 
-        public Lanche(int id, string nome, string descricaoCurta, string descricaoDetalhada, double preco, string imagemUrl, string imagemThumbnailUrl, bool isLanchePreferido, bool emEstoque, Categoria categoria)
+        public Lanche(int id, string nome, string descricaoCurta, string descricaoDetalhada, decimal preco, string imagemUrl, string imagemThumbnailUrl, bool isLanchePreferido, bool emEstoque, Categoria categoria)
         {
             Id = id;
             Nome = nome;
