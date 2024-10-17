db2986cf03f743f770ae45d0e87db7b02e32c5b3
Camilo LR <autumndrums@hotmail.com>
2024-10-17 17:00:33 -0300

    ViewData, ViewBag e TempData em Lanche

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -14,7 +14,16 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
+            ViewData["Titulo"] = "Todos os Lanches";
+            ViewData["Data"] = DateTime.Now;
+
             var lanches = _lancheRepository.Lanches;
+            var totalLanches = lanches.Count();
+
+            ViewBag.Total = "Total de Lanches : ";
+            ViewBag.TotalLanches = totalLanches;
+
+
             return View(lanches);
         }
     }
