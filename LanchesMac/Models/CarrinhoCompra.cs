265e6d449fa56392b177133b959c368c38c76b74
Camilo LR <autumndrums@hotmail.com>
2024-10-23 14:46:46 -0300

    Classe CarrinhoCompra, com Session e metodo Retorna Carrinho de Compra

---
+++ LanchesMac/Models/CarrinhoCompra.cs
@@ -0,0 +1,41 @@
+﻿using LanchesMac.Context;
+
+namespace LanchesMac.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+
+        //---> Retorna um carrinho de compra
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            //Difine uma sessão
+            ISession session = 
+                services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //Obtem um serviço do tipo do nosso contexto
+            var context = services.GetService<AppDbContext>();
+
+            //Obtem ou gera Id do carrinho - Se não existir, eu crio
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            //Atribui o id do carrinho na sessão
+            session.SetString("CarrinhoId", carrinhoId);
+
+            //Retorna o carrinho com o contexto e o Id atribuido ou obtido
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }
+
+    }
+}
