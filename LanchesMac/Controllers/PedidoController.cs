487e31751a5b1043ba2ac0ca3310d6eafe63220e
Camilo LR <autumndrums@hotmail.com>
2024-11-20 16:00:25 -0300

    Metodo IActionResult Checkout que processa os dados do pedidos para exibir seus detalhes

--- LanchesMac/Controllers/PedidoController.cs
+++ LanchesMac/Controllers/PedidoController.cs
@@ -25,10 +25,55 @@ namespace LanchesMac.Controllers
             return View();
         }
 
+        //Método que apresenta todas as informações de um pedido
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            //Obtem os itens do carrinho e compra do cliente
+            List<CarrinhoCompraItem> itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            //Verifica se existem itens de pedido
+            if(_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio");
+            }
+
+            //Calcula o total de itens e o total do pedido
+            foreach(var item in itens)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+
+            }
+
+            //Atribui os valores obtidos ao pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            //Valida os dados do pedido
+            if(ModelState.IsValid)
+            {
+                //Cria o pedido e os detalhes
+                _pedidoRepository.CriarPedido(pedido);
+
+                //Define mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :)";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //Limpa o carrinho do cliente
+                _carrinhoCompra.LimparCarrinho();
+
+                //Exibe a view com dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+
+            }
+            //Se for inválido, retorna a view com os dados do pedido
+            return View(pedido);
+
         }
     }
 }
