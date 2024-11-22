0207a82c38f1a4c6e1012d4a8c43fb268e03f623
Camilo LR <autumndrums@hotmail.com>
2024-11-22 14:32:58 -0300

    Metodo Register user em AccountController

--- LanchesMac/Controllers/AccountController.cs
+++ LanchesMac/Controllers/AccountController.cs
@@ -47,5 +47,35 @@ namespace LanchesMac.Controllers
             ModelState.AddModelError("", "Falha ao realizar o login!!");
             return View(loginVM);
         }
+
+        
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //Processa o post do formulário de cadastro de usuário
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginViewModel registroVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = registroVM.UserName };
+                var result = await _userManager.CreateAsync(user, registroVM.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    this.ModelState.AddModelError("Registro", "Falha ao registrar o usuário");
+                }
+            }
+            return View(registroVM);
+        }
+
+
     }
 }
