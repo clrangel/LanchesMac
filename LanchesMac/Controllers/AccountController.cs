c9abc3a8967533dc204459ac9bc09067bca0edbe
Camilo LR <autumndrums@hotmail.com>
2024-11-22 16:10:42 -0300

    Implementando o logout, com partialview

--- LanchesMac/Controllers/AccountController.cs
+++ LanchesMac/Controllers/AccountController.cs
@@ -76,6 +76,18 @@ namespace LanchesMac.Controllers
             return View(registroVM);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.User = null;
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
 
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
     }
 }
